41a711a20bbb696ad7c5b612b4327fd3dba578e0
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-11 14:00:45 +0000

    DATA TYPE

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// alert("hello") --> we are using node.js not browser DOM
+
+/* DATA TYPE
+
+number = 2^53
+bigint
+string = ""
+boolean = 1/0
+null = standalone value
+undefined
+
+*/
+
+
+
+
+/* 
+1. "use strict" let the js compiler tell that we are following new JS version coding style
+2. ";" use to terminate line inorder to increase code readibility
+3.  
+
+*/
