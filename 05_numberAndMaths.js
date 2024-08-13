8b8002f44203e6a49942f833a6bfd5f8f812b1fb
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-13 12:15:20 +0000

    maths

--- 05_numberAndMaths.js
+++ 05_numberAndMaths.js
@@ -17,4 +17,29 @@ let num3 = 1000000
 console.log(num3.toLocaleString());
 console.log(num3.toLocaleString('en-IN'));
 
+/* MATHs */
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.min(3,5,6,7));
+console.log(Math.max(3,5,6,7));
+console.log(Math.random()); //between 0 and 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
 
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+/*
+
+1. Number is an object to hold decimal
+2. console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+    Formula to genrate random number in a range.
+
+
+*/
