aeb02ac485dfed1e702663e1d91a6383bce6a0c6
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-13 12:30:32 +0000

    date & time

--- 06_dateAndTime.js
+++ 06_dateAndTime.js
@@ -7,3 +7,16 @@ console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toLocaleString());
 
+let myDate2 = new Date("01-03-2024")
+console.log(myDate2.toLocaleString());
+
+let myDate3 = Date.now();
+console.log(myDate3);
+console.log(Math.floor(Date.now()/1000)); // ms to sec
+
+
+let myDate4 = new Date()
+console.log(myDate4);
+console.log(myDate4.getDay());
+console.log(myDate4.getMonth());
+console.log(myDate4.getFullYear());
