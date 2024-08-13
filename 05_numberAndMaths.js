80f783a208094eef915b80bda408aae87507e980
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-13 03:42:39 +0000

    ./

---
+++ 05_numberAndMaths.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score);
+
+
+const age = new Number(400)
+console.log(age);
+
+console.log(age.toString());
+console.log(age.toFixed(3));
+
+let num2 = 23.6689
+console.log(num2.toPrecision(3));
+num2 = 123.6689
+console.log(num2.toPrecision(3));
+
+let num3 = 1000000
+console.log(num3.);
+
+
