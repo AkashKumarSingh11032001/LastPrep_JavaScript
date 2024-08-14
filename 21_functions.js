c7903ec52d93d4fe69137fdafce0eb3fe8a83250
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-14 11:01:12 +0530

    function

---
+++ 21_functions.js
@@ -0,0 +1,43 @@
+function addTwo(x,y) {
+    return x+y;
+}
+
+let addVal = addTwo(5,10);
+console.log(addVal);
+
+/* function with object and array */
+
+// rest oprator -- [Array]
+function addNumbers(...numbers) {
+    console.log("Rest opr: ",numbers);
+    
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total;
+}
+
+console.log(addNumbers(50,60,70));
+
+// object
+function handleObj(obj){
+    console.log("Object: ",obj);
+    console.log(`name ${obj.name}, age ${obj.name}`);
+}
+
+let obj = {
+    name: "Akash",
+    age:24,
+}
+handleObj(obj);
+handleObj({
+    name: "Singh",
+    age: 54,
+});
+
+
+/*
+1.
+
+*/
