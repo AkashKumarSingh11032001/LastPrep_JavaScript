6392279a25490f4d9b3ecbd148b9cd84a3875c2c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-14 14:57:49 +0530

    conditional flow

---
+++ 31_controlFlow.js
@@ -0,0 +1,38 @@
+/* +++++++ IF ELSE IF +++++++ */
+
+const isLoggedIn = true;
+if (isLoggedIn) {
+  console.log("user logged in!");
+} else {
+  console.log("user not logged in!");
+}
+
+const temperature = 30;
+if (temperature == "30") console.log("== PAss: compare without datatype");
+else console.log("== fail");
+
+if (temperature === "30") console.log("=== PAss");
+else console.log("=== fail: compare with datatype");
+
+/* +++++++ SWITCH CASE +++++++ */
+let key = 3
+switch (key) {
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+    console.log(2);
+    break;
+  case 3:
+    console.log(3);
+    break;
+
+  default:
+    break;
+}
+
+/*
+1. COMPARISION OPR: <, >, <=, >=, ==, !=, ===, !==
+2. Logical OPR: &&, ||, !
+
+*/
