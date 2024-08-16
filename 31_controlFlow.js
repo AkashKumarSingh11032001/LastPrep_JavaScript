161961be0e2eb30044357f2f17e5b3880d9042f9
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-16 10:27:00 +0530

    control flow

--- 31_controlFlow.js
+++ 31_controlFlow.js
@@ -32,11 +32,29 @@ switch (key) {
     break;
 }
 
+// Nullish Coalescing Oprator(??): null undefined
+let val1
+val1 = 5 ?? 10
+console.log(val1);
+let val2
+val2 = null ?? 10
+console.log(val2);
+let val3
+val3 = undefined ?? 10
+console.log(val3);
+let val4
+val4 = null ?? 15 ?? 10
+console.log(val4);
+
+
+
 /*
 1. COMPARISION OPR: <, >, <=, >=, ==, !=, ===, !==
 2. Logical OPR: &&, ||, !
 3. truthy and falsy value
   Falsey: false,0,-0,BigInt 0n, "",null,undefined, NaN
   Truthy: "0", " ", [], {}, function(){}
+4. Nullish Coalescing Oprator
+5. teneray oprator is diffrent from Nullish Coalescing Oprator 
 
 */
