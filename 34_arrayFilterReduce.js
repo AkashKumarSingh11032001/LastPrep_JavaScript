eda541af1464ab944f687ce0812107e3948b2d04
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-16 15:29:05 +0530

    filter && reduce

---
+++ 34_arrayFilterReduce.js
@@ -0,0 +1,37 @@
+const lang = ["Python", "C++", "JavaScript", "Bash", "C"];
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Filter
+const filterLang = myNum.filter((num) => num > 4);
+console.log(filterLang);
+
+const filterLang2 = myNum.filter((num) => {
+  return num > 4;
+});
+console.log(filterLang2);
+
+const json = [
+  { name: "Akash", age: 20 },
+  { name: "Kumar", age: 21 },
+  { name: "Singh", age: 22 },
+];
+
+let jsonkeys = json.filter((key)=> key.name === 'Kumar')
+console.log(jsonkeys);
+
+jsonkeys = json.filter((key) => {
+    return key.name === 'Kumar' && key.age >= 21
+})
+console.log(jsonkeys);
+
+
+ 
+// Reduce
+const reduceNum = myNum.reduce((acc, curr) => acc + curr);
+console.log(reduceNum);
+
+/* 
+1. map and for-each will not return anythings where as filter and reduce will return some value.
+2. 
+
+*/
