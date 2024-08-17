fb3e2d8e964e79dbc4286ad47aaf2393df089883
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-17 13:45:20 +0530

    ./

--- INTERMIDIATE JAVASCRIPT/42_createElement.js
+++ INTERMIDIATE JAVASCRIPT/42_createElement.js
@@ -1,1 +1,37 @@
-const parent = document.querySelector(".parent")
+const parent = document.querySelector(".parent");
+console.log(parent);
+
+console.log(parent.children);
+for (let i = 0; i < parent.children.length; i++) {
+  console.log(parent.children[i].innerHTML);
+  parent.children[i].style.color = "blue";
+}
+console.log(parent.firstElementChild);
+console.log(parent.lastElementChild);
+
+const day1 = document.querySelector(".day");
+console.log(day1);
+day1.style.color = "Red";
+
+console.log(day1.parentElement);
+console.log(day1.nextElementSibling);
+console.log(day1.nextElementSibling.nextElementSibling);
+
+console.log("NODES: ", parent.childNodes);
+
+//  create new element
+const newElement = document.createElement("h1");
+newElement.innerHTML = "Hello World";
+parent.appendChild(newElement);
+console.log(parent.children);
+console.log(parent.childNodes);
+//  remove element
+parent.removeChild(parent.children[0]);
+console.log(parent.children);
+
+//  replace element
+const newElement2 = document.createElement("h1");
+newElement2.innerHTML = "Hello World 2";
+parent.replaceChild(newElement2, parent.children[0]);
+console.log(parent.children);
+
