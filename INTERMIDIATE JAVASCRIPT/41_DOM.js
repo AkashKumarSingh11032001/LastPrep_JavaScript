f804803c2218b1c30461a3594f93f88d76301127
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-17 10:10:53 +0530

    querySelector

--- INTERMIDIATE JAVASCRIPT/41_DOM.js
+++ INTERMIDIATE JAVASCRIPT/41_DOM.js
@@ -22,5 +22,18 @@ title.innerContent
 // 'Learning DOM ~ With AKS'
 title.innerHTML
 
+// querySelector
+document.querySelector('h1')
+// <h1 id=​"first" class=​"heading">​Learning DOM​</h1>​
+document.querySelector('p')
+// <p id=​"second">​Learning DOM with AKS​</p>​
+document.querySelector('h1').innerText
+// 'Learning DOM'
+document.querySelector('h1').style.backgroundColor = 'red'
+// background changed
+document.querySelector('.heading')
+// <h1 id=​"first" class=​"heading">​Learning DOM
+
+
 
 
