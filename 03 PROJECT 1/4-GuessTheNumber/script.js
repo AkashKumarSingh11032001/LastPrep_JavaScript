592f0a954f1c1e87156873aec3af49c3acf20762
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-18 11:22:54 +0530

    guess game inprogress

---
+++ 03 PROJECT 1/4-GuessTheNumber/script.js
@@ -0,0 +1,65 @@
+const randomNumber = parseInt((Math.random() * 100) + 1)
+// console.log(randomNumber);
+
+const submit = document.querySelector("#subt")
+const userInput = document.querySelector("#guessField")
+const guessSlot = document.querySelector(".guesses")
+const remaining = document.querySelector(".lastResult")
+const lowOrHigh = document.querySelector(".lowOrHi")
+const startOver = document.querySelector(".resultParas")
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let remainingGuesses = 10
+let numGuess = 1
+let playGame = true
+
+if(playGame){
+    submit.addEventListener("click", (e) => {
+        e.preventDefault()
+        const userInput = parseInt(userInput.value)
+        validateGuess(userInput)
+        displayGuess(userInput)
+        displayMessage(userInput)
+        newGame()
+        endGame()
+    })
+}
+
+// value validation check
+function validateGuess(guess){
+    if (guess === "" || isNaN(guess) || guess < 1 || guess > 100) {
+        alert("Please enter a number between 1 and 100")
+        userInput.value = ""
+        userInput.focus()
+        return false
+    }
+}
+
+// guess ==== randomNumber
+function checkGuess(guess){
+    if (guess === randomNumber) {
+        
+    }
+}
+
+// clean input and update the variable
+function displayGuess(guess){
+
+}
+
+// display n update dom
+function displayMessage(message){
+
+}
+
+// new game
+function newGame(){
+
+}
+
+// end game
+function endGame(){
+    
+}
