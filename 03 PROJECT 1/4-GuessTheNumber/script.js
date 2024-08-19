9fa3c6c9d062f9ac6bf0101d2b3497684f4ab0a0
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-19 08:38:12 +0530

    checkGuess

--- 03 PROJECT 1/4-GuessTheNumber/script.js
+++ 03 PROJECT 1/4-GuessTheNumber/script.js
@@ -1,72 +1,72 @@
-const randomNumber = parseInt((Math.random() * 100) + 1)
+const randomNumber = parseInt(Math.random() * 100 + 1);
 // console.log(randomNumber);
 
-const submit = document.querySelector("#subt")
-const userInput = document.querySelector("#guessField")
-const guessSlot = document.querySelector(".guesses")
-const remaining = document.querySelector(".lastResult")
-const lowOrHigh = document.querySelector(".lowOrHi")
-const startOver = document.querySelector(".resultParas")
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowOrHigh = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
 
-const p = document.createElement('p')
+const p = document.createElement("p");
 
-let prevGuess = []
-let remainingGuesses = 10
-let numGuess = 1
-let playGame = true
+let prevGuess = [];
+let remainingGuesses = 10;
+let numGuess = 1;
+let playGame = true;
 
-if(playGame){
-    submit.addEventListener("click", (e) => {
-        e.preventDefault()
-        const userInput = parseInt(userInput.value)
-        validateGuess(userInput)
-        displayGuess(userInput)
-        displayMessage(userInput)
-        newGame()
-        endGame()
-    })
+if (playGame) {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const userInput = parseInt(userInput.value);
+    validateGuess(userInput);
+    displayGuess(userInput);
+    displayMessage(userInput);
+    newGame();
+    endGame();
+  });
 }
 
 // value validation check
-function validateGuess(guess){
-    if (guess === "" || isNaN(guess) || guess < 1 || guess > 100) {
-        alert("Please enter a number between 1 and 100")
-        userInput.value = ""
-        userInput.focus()
-        return false
+function validateGuess(guess) {
+  if (guess === "" || isNaN(guess) || guess < 1 || guess > 100) {
+    alert("Please enter a number between 1 and 100");
+    userInput.value = "";
+    userInput.focus();
+    return false;
+  } else {
+    prevGuess.push(guess);
+    if (numGuess === 11) {
+      displayGuess(guess);
+      displayMessage("Game Over");
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
     }
-    else{
-        prevGuess.push(guess)
-        if(numGuess === 11){
-            displayGuess(guess)
-            displayMessage("Game Over")
-        }
-    }
+  }
 }
 
 // guess ==== randomNumber
-function checkGuess(guess){
-    if (guess === randomNumber) {
-        
-    }
+function checkGuess(guess) {
+  if (guess === randomNumber) {
+    displayGuess("You Won!, You Guessed Right");
+    endGame();
+  } else if (guess < randomNumber) {
+    displayGuess("Too Low");
+  } else if (guess > randomNumber) {
+    displayGuess("Too High");
+  }
 }
 
 // clean input and update the variable
-function displayGuess(guess){
-
-}
+function displayGuess(guess) {}
 
 // display n update dom
-function displayMessage(message){
-
-}
+function displayMessage(message) {}
 
 // new game
-function newGame(){
-
-}
+function newGame() {}
 
 // end game
-function endGame(){
-    
-}
+function endGame() {}
