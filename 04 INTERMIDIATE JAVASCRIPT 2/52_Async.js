f3a07b57f6e6cbe8fe9b885cccd465e14159b786
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-19 21:06:49 +0530

    async js

---
+++ 04 INTERMIDIATE JAVASCRIPT 2/52_Async.js
@@ -0,0 +1,8 @@
+/* Keynotes on Async Javascript
+ 1. Javascript is synchronous
+ 2. Javascript is single thread
+ 3. Javascript is non blocking
+ 4. Javascript is event driven
+ 5. In Javascript each execution will wait for the last one to complete before next operation
+
+*/
