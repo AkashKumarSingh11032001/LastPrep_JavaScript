a00b55b145e4294dc5327c576bc9204efc66a906
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-21 08:47:03 +0530

    ./

--- 05 PROJECT 2/61_project1.js
+++ 05 PROJECT 2/61_project1.js
@@ -4,6 +4,16 @@ setTimeout(()=>{
     
 }, 1000)
 
+const func1 = () => {
+    console.log("Hello Bye!");
+}
+const func2 = () => {
+    console.log("Hello Welcome!");
+}
+
+setTimeout(func1,1000)
+setTimeout(func2,1000)
+
 
 /*
 1. setTimeOut(<refrence>,<timeout>)
