c99a2deb64a282b9dc38f97bc09ad9d34f85d096
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-22 16:37:24 +0530

    LimitlessColor Project Done

--- 05 PROJECT 2/6-unlimitedColors/script.js
+++ 05 PROJECT 2/6-unlimitedColors/script.js
@@ -1,33 +1,32 @@
 // genrate random hex color
 const randomColor = () => {
-    const hex = "0123456789ABCDEF"
-    let color = '#';
-    for(let i = 0; i < 6; i++){
-        color += hex[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 // console.log(randomColor());
 
-
-let holdX
-document.querySelector('#start').addEventListener('click',() => {
-    // console.log('clicked');
-    // let colors = ['red','blue','green','yellow','pink','purple','orange','black','white','gray'];
-    holdX = setInterval(()=>{
-        let randomColor = randomColor();
-        // let randomColor = colors[Math.floor(Math.random() * colors.length)];
-        document.querySelector('body').style.backgroundColor = randomColor;
-        document.querySelector('#color').innerHTML = randomColor;
-
-    },1000)
-    
-})
-document.querySelector('#stop').addEventListener('click',()=>{
-    clearInterval(holdX)
-    holdX = null
-    document.querySelector('body').style.backgroundColor = 'white';
-    document.querySelector('#color').innerHTML = 'white';
-    documentquerySelector('#color').style.color = 'black';
-})
+let holdX;
+document.querySelector("#start").addEventListener("click", () => {
+  // console.log('clicked');
+  // let colors = ['red','blue','green','yellow','pink','purple','orange','black','white','gray'];
+  if (!holdX) {
+    holdX = setInterval(() => {
+      let randomColorx = randomColor();
+      // let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      document.querySelector("body").style.backgroundColor = randomColorx;
+      document.querySelector("#color").innerHTML = randomColorx;
+    }, 1000);
+  }
+});
+document.querySelector("#stop").addEventListener("click", () => {
+  clearInterval(holdX);
+  holdX = null;
+  document.querySelector("body").style.backgroundColor = "white";
+  document.querySelector("#color").innerHTML = "white";
+  document.querySelector("#color").style.color = "black";
+});
