e0cd9dc5c973a9d4e472fdbc8c386181ebaa944e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-22 17:04:06 +0530

    AJAX

---
+++ 06 ADVANCED JAVASCRIPT 1/71_APIRequest.js
@@ -0,0 +1,32 @@
+// +++ AJAX +++
+// AJAX = Asynchronous JavaScript and XML
+// AJAX is a technique used to communicate with a server and update parts of a web page without reloading the whole page.
+
+// +++ XMLHttpRequest+++
+// XMLHttpRequest is an object that allows you to communicate with servers.
+// The XMLHttpRequest object can be used to exchange data with a server behind the scenes.
+
+// +++ API +++
+// API = Application Programming Interface
+// API is a set of rules and protocols for building and interacting with software applications.
+// API is a way for different software to communicate with each other.
+
+// +++ API Request +++
+// API Request is a request made to a server to retrieve data.
+// API Request is made using the XMLHttpRequest object.
+// API Request can be made using GET or POST method.
+// GET method is used to retrieve data from a server.
+// POST method is used to send data to a server.
+
+// +++ Example +++
+// Example of API Request using XMLHttpRequest object.
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://randomuser.me/api/');
+// Example of API Request using fetch() method.
+// Example of API Request using axios library.
+
+
+
+/*
+1. API is a medium to help two sytem written in different language to communicate.
+*/
