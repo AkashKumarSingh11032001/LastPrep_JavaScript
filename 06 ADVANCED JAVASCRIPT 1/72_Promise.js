47c7483a900e7c8893033cf6c33ee71768341402
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-22 18:44:45 +0530

    Promise way 2

--- 06 ADVANCED JAVASCRIPT 1/72_Promise.js
+++ 06 ADVANCED JAVASCRIPT 1/72_Promise.js
@@ -10,6 +10,19 @@ promise1.then(() => {
     console.log('Promise 1 Consumed');
 })
 
+// Promise way 2
+new Promise((resolve,reject)=>{
+// do work
+  setTimeout(() => {
+    console.log("Async Task 2 Accomplished");
+    resolve();
+  }, 1000);
+
+}).then(()=>{
+    console.log('Promise 2 Consumed')
+})
+
+
 /* "Async & await" and "Promise" are to handle Asynchronous programming in JS
 
 1. A Promise is an object that may produce a single value sometime in the future: either a resolved value if it succeeds, or a reason that it failed if it doesn't. A Promise may be in one of these states:
