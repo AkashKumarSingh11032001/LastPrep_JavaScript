1abdd2ad253ff206f5497bdc96120c8063dfbe09
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-25 20:13:05 +0530

    class and object

--- 06 ADVANCED JAVASCRIPT 1/77_Class.js
+++ 06 ADVANCED JAVASCRIPT 1/77_Class.js
@@ -24,4 +24,24 @@ chai.logout();
 console.log(chai.encryptPassword());
 
 
-// behind ceane
+// behind Sceane ---> without class 
+function UserX(uname,email,pass){
+    this.uname=uname
+    this.email=email
+    this.pass=pass
+
+}
+UserX.prototype.login = function(){
+    console.log(`${this.uname.toUpperCase()} is logged in`);
+}
+UserX.prototype.logout = function(){
+    console.log(`${this.uname.toUpperCase()} is logged out`);
+}
+UserX.prototype.encryptPassword = function(){
+    return `${this.pass}abc`
+}
+
+const uname1 = new UserX("AKS", "AKS@gmail.com", "123")
+uname1.login();
+uname1.logout();
+console.log(uname1.encryptPassword());
