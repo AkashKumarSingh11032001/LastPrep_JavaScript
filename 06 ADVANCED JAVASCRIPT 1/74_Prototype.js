e17c8289de62d245c9a1a41bc45e40ea9c321b3a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-25 19:02:02 +0530

    PROTOTYPE

--- 06 ADVANCED JAVASCRIPT 1/74_Prototype.js
+++ 06 ADVANCED JAVASCRIPT 1/74_Prototype.js
@@ -1,1 +1,39 @@
 /* PROTOTYPE   */
+// Array -> object -> null
+// string -> object -> null
+
+function multiply(num) {
+    return num*5
+}
+
+// function is object? --- Yes
+console.log(multiply(5));
+
+multiply.power = 5
+console.log(multiply.power); //--> 5
+
+console.log(multiply.prototype); //--> {}
+
+
+// eg2
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+
+
+
+// Array.prototype.multiply = function() {
+//     for (let i = 0; i < this.length; i++) {
+//         this[i] = this[i]*5
+//     }
+//     return this
+// }
+
+// const arr = [1,2,3,4,5]
+// arr.multiply()
+// console.log(arr)
+
+// const str = "hello"
+// str.multiply()
