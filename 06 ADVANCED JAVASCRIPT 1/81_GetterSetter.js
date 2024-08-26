11b677572bce544d66ef5d37751d15d15bbb9b1c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-26 10:23:02 +0530

    setterGetter

--- 06 ADVANCED JAVASCRIPT 1/81_GetterSetter.js
+++ 06 ADVANCED JAVASCRIPT 1/81_GetterSetter.js
@@ -4,24 +4,38 @@ class User{
         this.Pass = Pass
     }
 
-    // getter
-    get Pass(){
-        return this._Pass.toUpperCase() //--> create new variable
+    /* EMAIL */
+    set email(newEmail){
+        this._email = newEmail.toLowerCase() //--> create new variable
+    }
+    get email(){
+        return this._email.toUpperCase() //--> create new variable
     }
 
+    /* Pasword */
     // setter
     set Pass(newPass){
         this._Pass = newPass.toLowerCase() //--> create new variable
     }
 
+    // getter
+    get Pass(){
+        return this._Pass.toUpperCase() //--> create new variable
+    }
+
+
 }
 
 const user1 = new User("AKS@gmail.com","123")
 console.log(user1.Pass);
-user1.Pass = "1234"
+user1.Pass = "abc"
 console.log(user1.Pass);
+
+console.log(user1.email);
+user1.email = "abc@gmail.com"
+console.log(user1.email);
 
 
 /*
-1. both getter and setter need to define together
+1. both getter and setter need to define together with same name
 */
