724e7be67bcd9bb0bebd89b71e029e4c6b565b67
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-26 10:34:54 +0530

    Getter Setter

--- 06 ADVANCED JAVASCRIPT 1/81_GetterSetter.js
+++ 06 ADVANCED JAVASCRIPT 1/81_GetterSetter.js
@@ -1,3 +1,5 @@
+//* NEW APPROCH */
+
 class User{
     constructor(email,Pass){
         this.email = email
@@ -26,6 +28,8 @@ class User{
 
 }
 
+console.log("+++ CLASS BASED APPROCH +++");
+
 const user1 = new User("AKS@gmail.com","123")
 console.log(user1.Pass);
 user1.Pass = "abc"
@@ -35,7 +39,79 @@ console.log(user1.email);
 user1.email = "abc@gmail.com"
 console.log(user1.email);
 
+/* OLD APPROCH */
+function UserX(email,pass){
+    this._email = email;
+    this._pass = pass
+
+    // setter and Getter
+    Object.defineProperty(this,'email',{
+        get:function(){
+            return this._email.toUpperCase()
+        },
+        set:function(value){
+            this._email = value.toLowerCase()
+        }
+    }) //-->setter
+
+    // Password
+    Object.defineProperty(this,'pass',{
+        get:function(){
+            return this._pass.toUpperCase()
+        },
+        set:function(value){
+            this._pass = value.toLowerCase()
+        }
+    })
+}
+
+console.log("+++ FUNCTION BASED APPROCH  +++");
+
+const user2 = new UserX("aks@gmail.com","123")
+console.log(user2.pass);
+user2.pass = "abc"
+console.log(user2.pass);
+
+console.log(user2.email);
+user2.email = "abc@gmail.com"
+console.log(user2.email);
+
+
+// --> OBJECT BASED SYNTAX
+const UserXX = {
+    _email: 'aks@gmail.com',
+    _pass: '123',
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+    set email(value){
+        this._email = value.toLowerCase()
+    },
+
+    get pass(){
+        return this._pass.toUpperCase()
+    },
+    set pass(value){
+        this._pass = value.toLowerCase()
+    }
+
+}
+
+console.log("+++ OBJECT BASED APPROCH  +++");
+
+console.log(UserXX.pass);
+UserXX.pass = "abc"
+console.log(UserXX.pass);
+
+console.log(UserXX.email);
+UserXX.email = "abc@gmail.com"
+console.log(UserXX.email);
+
+
+
 
 /*
 1. both getter and setter need to define together with same name
 */
+ 
