5b19788c1fdef7b682be1bb047b0b4ba493b88c1
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-26 11:06:55 +0530

    clouser

--- 06 ADVANCED JAVASCRIPT 1/82_lexicalScopingAndClosure.js
+++ 06 ADVANCED JAVASCRIPT 1/82_lexicalScopingAndClosure.js
@@ -6,6 +6,7 @@ A closure is a function that remembers its outer variables and can access them. 
 Lexical scoping is a convention in JavaScript where the scope of a variable is determined by its location in the code. It means that a variable can only be accessed within the block or function in which it is declared.
 
 */
+console.log("+++ Lexical scoping +++");
 
 function outerFunction() {
     let outerVariable = 'I am outside!';
@@ -24,3 +25,38 @@ function outerFunction() {
 
 
 outerFunction();
+
+//--> Clouser
+console.log("+++ Closure +++");
+
+function outerFunction() {
+    let outerVariable = 'I am outside!';
+
+    function innerFunction() {
+        console.log(outerVariable);
+    }
+    return innerFunction; //--> it will return entier 'lexical scope'
+}
+
+const myFunction = outerFunction();
+myFunction();
+
+//-> practical explanation
+
+// document.getElementById("orange").onclick = () => {
+//     document.body.style.backgroundColor = 'orange'
+// }
+// document.getElementById("green").onclick = () => {
+//     document.body.style.backgroundColor = 'green'
+// }
+
+function clickHandler(color){
+    // document.body.style.backgroundColor = `${color}` //-->
+    return function(){
+        document.body.style.backgroundColor = `${color}`
+    }
+
+}
+
+document.getElementById("orange").onclick = clickHandler("orange")
+document.getElementById("green").onclick = clickHandler("green")
