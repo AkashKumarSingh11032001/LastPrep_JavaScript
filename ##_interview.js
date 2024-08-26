9f0922f2514d980620e22b3e948feea66e1a018a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2024-08-26 08:48:37 +0530

    mathPI

--- ##_interview.js
+++ ##_interview.js
@@ -27,6 +27,49 @@
     : Stack (all primitive type use stack) / call by value
       Heap (all Non primitive type use heap) / call by reference
       
-6. value of math.pie === 3.14? can we round of to 4?
+6. value of math.pie === 3.14? can we round of to 4? //--> NO
 
 */
+
+
+//--> Answer 6
+
+console.log(Math.PI);
+Math.PI = 4 //--> will not work, because PI is constant
+console.log(Math.PI);
+
+const desc = Object.getOwnPropertyDescriptor(Math, "PI")
+console.log(desc);
+console.log(desc.writable); //--> false [will not allowed to overwrite]
+
+
+//--> creating our own "getOwnPropertyDescriptor"
+const chai = {
+    name: "ginger Chai",
+    price: 300,
+    isAviable: true,
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+console.log(chai);
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+// change propert level
+Object.defineProperty(chai,"name",{
+    value: "ginger chai",
+    writable: false,
+    // configurable: true,
+    enumerable: false //--> will not list in object
+})
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+
+// we set enumerable to false so we can't iterate
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
+
+
